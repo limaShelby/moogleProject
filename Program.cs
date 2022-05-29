f074f8bbf3af52b77df681e720813a2cd21a959e
limaShelby <94007502+limaShelby@users.noreply.github.com>
2022-05-29 14:38:09 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+
+namespace Moogle_Project
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] myFilesDirectory = Directory.GetFiles("./Data");
+
+            MyFile file1 = new MyFile(myFilesDirectory[0]);
+
+            Console.WriteLine(file1.GetTF_IDF("fox"));
+        }
+    }
+}
